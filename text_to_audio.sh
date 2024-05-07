e0ce4f9eb8585fda718a019ed7ba0632d4150a37
karsa <wanghongtao114@gmail.com>
2024-05-07 20:38:45 +0800

    add file

---
+++ text_to_audio.sh
@@ -0,0 +1,8 @@
+# 设置要遍历的目录路径
+dic=`pwd`
+for file in "$dic"/*; do
+    if [ -f "$file" ]; then
+        edge-tts --voice zh-CN-YunxiNeural  --rate +40% -f $file --write-media $file.mp3 --write-subtitles $file.tts;
+        # 在这里可以对文件执行你想要的操作
+    fi
+done
